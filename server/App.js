530cad8635fa0f7a68ea6db71ec224c794310875
S4N <dcruzhe@unal.edu.co>
2023-05-24 22:04:45 -0500

    Test Vercel [FIXED CORS v2]

--- server/App.js
+++ server/App.js
@@ -9,15 +9,26 @@ const cors = require('cors');
 const app = express();
 const port = 5000;
 
+app.use(cors({
+  origin:['http://localhost:3000','http://127.0.0.1:3000'],
+  credentials:true
+}));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors({ origin: true }));
+/*app.use(cors({ origin: true }));*/
 app.use(express.json());
 
 
 app.use('/api',  routes)
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+  res.header('Access-Control-Allow-Headers', true);
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  next();
+});
 app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
 })
