27c0f98b113a1b0a8884ba859c92e0652f32366a
S4N <dcruzhe@unal.edu.co>
2023-05-25 19:40:16 -0500

    Test Vercel [REVERT CORS v3 + Change 2FA Async + NO TIME ]

--- src/Pages/2FA/TwoFAFunction.js
+++ src/Pages/2FA/TwoFAFunction.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
-export const doVerification2FA = async (formData) => {
+export const doVerification2FA = (formData) => {
     console.log(process.env.REACT_APP_API)
-    await axios.post(process.env.REACT_APP_API+'/send2FA', formData)
+    axios.post(process.env.REACT_APP_API+'/send2FA', formData)
         .then(
             /*function (res){
                 console.log(res.data)
                 console.log("dentrooo")
             }*/
+
         )
         .catch(err => console.log(err));
 
